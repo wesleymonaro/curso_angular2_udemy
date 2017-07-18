adef1c28aa30ed4569d3cc3ffdef637d262b7374
Wesley Monaro <wesleymonaro@hotmail.com>
2017-07-18 01:15:21 -0300

    Aula 31 - Component master

---
+++ angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    moduleId : module.id,
+    selector : "contato-detalhe",
+    templateUrl : "contato-detalhe.component.html"
+})
+export class ContatoDetalheComponent{}
