5c92963869359d9407f675381550961ffa2ad206
Wesley Monaro <wesleymonaro@hotmail.com>
2017-07-18 00:48:34 -0300

    added contact array

---
+++ angular2/01-cadastro-contatos/app/contatos/contatos-mock.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.CONTATOS = [
+    { id: 1, nome: 'Seu madruga', email: 'email@email.com', telefone: '(00) 0000-0000' },
+    { id: 2, nome: 'Kiko', email: 'kiko@email.com', telefone: '(00) 0000-0000' },
+    { id: 3, nome: 'Chaves', email: 'chaves@email.com', telefone: '(00) 0000-0000' },
+    { id: 4, nome: 'Chiquinha', email: 'chiquinha@email.com', telefone: '(00) 0000-0000' },
+    { id: 5, nome: 'Sr. Barriga', email: 'barriga@email.com', telefone: '(00) 0000-0000' }
+];
+//# sourceMappingURL=contatos-mock.js.map
---
+++ angular2/01-cadastro-contatos/app/contatos/contatos-mock.ts
@@ -0,0 +1,9 @@
+import { Contato } from "./contato.model";
+
+export const CONTATOS: Contato[] = [
+    {id:1, nome : 'Seu madruga', email: 'email@email.com', telefone : '(00) 0000-0000'},
+    {id:2, nome : 'Kiko', email: 'kiko@email.com', telefone : '(00) 0000-0000'},
+    {id:3, nome : 'Chaves', email: 'chaves@email.com', telefone : '(00) 0000-0000'},
+    {id:4, nome : 'Chiquinha', email: 'chiquinha@email.com', telefone : '(00) 0000-0000'},
+    {id:5, nome : 'Sr. Barriga', email: 'barriga@email.com', telefone : '(00) 0000-0000'}
+]
