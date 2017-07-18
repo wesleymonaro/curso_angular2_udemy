98178a3cdecd25edf569975dca72abdb22b6d2fa
Wesley Monaro <wesleymonaro@hotmail.com>
2017-07-18 00:45:22 -0300

    create model

---
+++ angular2/01-cadastro-contatos/app/contatos/contato.model.js
@@ -0,0 +1,12 @@
+"use strict";
+class Contato {
+    constructor(id, nome, email, telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+}
+exports.Contato = Contato;
+//let c : Contato = new Contato(1, 'Fulano', 'teste@teste', '1234-5678'); 
+//# sourceMappingURL=contato.model.js.map
---
+++ angular2/01-cadastro-contatos/app/contatos/contato.model.ts
@@ -0,0 +1,11 @@
+export class Contato{
+
+    constructor(
+        public id : number,
+        public nome : string,
+        public email : string,
+        public telefone : string
+    ){}
+}
+
+//let c : Contato = new Contato(1, 'Fulano', 'teste@teste', '1234-5678');
