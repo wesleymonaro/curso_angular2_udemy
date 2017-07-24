1c50416530403546b795b35a3292f6d3c02159e7
Wesley Monaro <wesleymonaro@hotmail.com>
2017-07-24 16:49:47 -0300

    Aula 40 - Extraindo parâmetro da rota

--- angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.js
+++ angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.js
@@ -5,15 +5,36 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 const core_1 = require("@angular/core");
+const router_1 = require("@angular/router");
+const common_1 = require("@angular/common");
+const contato_service_1 = require("./contato.service");
 let ContatoDetalheComponent = class ContatoDetalheComponent {
+    constructor(contatoService, route, location) {
+        this.contatoService = contatoService;
+        this.route = route;
+        this.location = location;
+    }
+    ngOnInit() {
+        console.log("on init");
+        this.route.params.forEach((params) => {
+            let id = +params['id'];
+            console.log(id);
+        });
+    }
 };
 ContatoDetalheComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
         selector: "contato-detalhe",
         templateUrl: "contato-detalhe.component.html"
-    })
+    }),
+    __metadata("design:paramtypes", [contato_service_1.ContatoService,
+        router_1.ActivatedRoute,
+        common_1.Location])
 ], ContatoDetalheComponent);
 exports.ContatoDetalheComponent = ContatoDetalheComponent;
 //# sourceMappingURL=contato-detalhe.component.js.map
--- angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.ts
+++ angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.ts
@@ -1,8 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+import { Location } from "@angular/common";
+import { ContatoService } from "./contato.service";
 
 @Component({
     moduleId : module.id,
     selector : "contato-detalhe",
     templateUrl : "contato-detalhe.component.html"
 })
-export class ContatoDetalheComponent{}
+export class ContatoDetalheComponent implements OnInit{
+
+    constructor(
+        private contatoService : ContatoService,
+        private route : ActivatedRoute,
+        private location : Location
+    ){}
+    ngOnInit():void{
+        console.log("on init");
+        this.route.params.forEach((params : Params) => {
+            let id : number = +params['id'];
+            console.log(id);
+        })
+    }
+}
